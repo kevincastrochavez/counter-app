12be370b01671e3f87034dbd5c6c79276c92c9df
Kevin <kevin.castro.chavez0@gmail.com>
2022-09-21 11:41:55 -0600

    learned how to select views by id, change their text on a button click and show a toast

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.myBtn)
+        val textName = findViewById<TextView>(R.id.textName)
+        var timesClicked = 0
+
+        btnClickMe.setOnClickListener {
+            timesClicked += 1
+            textName.text = timesClicked.toString()
+            Toast.makeText(this, "Hey, I'm a toast!", Toast.LENGTH_LONG).show()
+        }
     }
 }
